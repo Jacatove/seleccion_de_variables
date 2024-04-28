4d9e4fcef84bdf2547c9df9cb8281ea0ec497ab6
Javier Torres <javiercamilotorresvera@gmail.com>
2024-04-28 21:08:13 +0200

    agregar comentarios.

--- stepwise.R
+++ stepwise.R
@@ -1,4 +1,5 @@
-# Instalamos el Paquete BAS, en el cual encontramos el dataset a usar, Hald Cement Data.
+# Instalamos el Paquete BAS, en el cual encontramos el dataset a usar,
+# Hald Cement Data.
 install.packages('BAS')
 library(BAS) # Usamos la librería.
 
@@ -8,76 +9,92 @@ data(Hald)
 # Miramos la estructura y el resumen del dataset.
 str(Hald)
 
-#################################################
-###### Algoritmo de Regresión Paso a paso  ######
-#################################################
-# 0. Se inicializan variables.
+################################################################################
+###### Algoritmo de Regresión Paso a paso  #####################################
+################################################################################
+# 1. Se inicializan variables.
   # regresores_modelo: lista vacía para agregar los regresores al modelo.
   # regresores_disponibles: lista con los regresores candidatos para el modelo.
-  # alpha: valor corte para determinar el valor crítico del t-estadístico.
+  # alpha_in: valor corte de entrada.
+  # alpha_out: valor corte de salida.
   # Y: respuesta.
 
-# 1. Se halla la mayor correlación simple entre un regresor X_j y la respuesta Y
-#  1.1. Se comparan la correlación entre todos los regresores en regresores_disponibles
-#.      contra la respuesta Y.
-# 2. Se hallan la mayores correlaciónes parciales
-## 2.1. Se determina la mejor correlación parcial con un nuevo,
-#.      dados los regresores del modelo, se reevalúan via el t-estadístico
-# 3. 
+# 2. Se halla la mayor correlación simple con la respuesta Y.
+# 2.1. Se compara la correlación entre todos los regresores en
+  #    regresores_disponibles con Y para determinar el regresor (Xk) con mayor
+  #    correlación con Y.
+# 2.2 Se verifica si el regresor (Xk) es significante para el nivel alpha_in
+  #   para agregarlo al modelo, de lo contrario se termina el algoritmo.
 
-# Definimos el valor Cutoff, (Alpha) para comprobar que el t-estadístico supera
-# el valor preseleccionado T_IN (T-to-enter).
-alpha <- 0.15
+# 3. Se hallan la mayores correlaciónes parciales.
+# 3.1. Se determina el regresor (Xj) con el mayor t-estadístico dados los
+#      regresores ya en el modelo.
+# 3.2 Se verifica si este regresor (Xj) es significante para el nivel alpha_in
+#   para agregarlo al modelo.
+
+# 4. Se reevalúa el modelo con la nueva variable agregada. Se determina el
+#    regresor con el menor t-estadístico y si dejó de ser significante par el
+#    nivel alpha_out, se remueve.
+
+
+
+# 1] Se inicializan variables.
+# Definimos las cotas de entrada y salida.
+alpha_in <- 0.15
+alpha_out <- 0.15
 regresores_modelo <- list() # El modelo inicia sin regresores.
-# names(Hald)  X1, X2, ..., Xn, Y.
 # Extraemos las variables regresoras de los nombres de las columnas, y removemos
 # la respuesta Y. Como names(Hald) retorna: [1] "X1" "X2" "X3" "X4" "Y", se
-# remueve el último elemento de la lista.
+# remueve el último elemento de la lista para sacar Y.
 regresores_disponibles <- names(Hald)[-length(names(Hald))]
-Y <- Hald$Y # Observaciones de la Respuesta.
+Y <- Hald$Y # Respuesta.
 
-# 1) Mayor correlación simple entre los regresores y la respuesta.
-mejor_regresor <- NULL # El regresor X_i con mejor correlación contra la respuesta Y.
+# 2] Se halla la mayor correlación simple con la respuesta Y.
+regresor_candidato <- NULL # X_k con mejor correlación con la respuesta Y.
 mejor_correlacion <- 0
 
-# Iteramos sobre las observaciones de las variables regresores.
 for (regresor_disponible in regresores_disponibles) {
-  X_i <- Hald[, regresor_disponible] # Valores del regresor Xi.
-  cor_y_xi <- cor(Y, X_i) # Covarianza entre Xi y la respuesta Y.
+  X_i <- Hald[, regresor_disponible] # Vector con los valores del regresor Xi.
+  cor_y_xi <- cor(Y, X_i) # Correlación entre Xi y la respuesta Y.
   if (abs(cor_y_xi) > abs(mejor_correlacion)){
     mejor_correlacion <- cor_y_xi
-    mejor_regresor <- regresor_disponible
+    regresor_candidato <- regresor_disponible
   }
 }
 
-formula_string <- paste("Y ~", sprintf("Hald$%s", mejor_regresor))
+formula_string <- paste("Y ~", sprintf("Hald$%s", regresor_candidato))
 
-# Regresión lineal.
-model <- lm(as.formula(formula_string))
+# Regresión lineal del regresor con mejor correlación con la respuesta Y.
+modelo <- lm(as.formula(formula_string))
 
 n <- nrow(Hald) # Número de observaciones.
-df <- n - 2 # grados de libertad, n observaciones menos dos parámetros. 
-valor_critico <- qt(1-alpha/2, df) # Valor Crítico
-t_valor <- abs(summary(model)$coefficients[, "t value"][2])
+df <- n - 2 # grados de libertad, n observaciones menos el parametro B0 y Bk.
+abs_t_valor <- abs(summary(modelo)$coefficients[, "t value"][2])
+T_in <- qt(1-alpha_in/2, df) # El valor crítico, la cota de entrada.
 
 # Aunque el regresor tenga la mejor correlación con la respuesta, solo se agrega
-# al modelo si existe relación lineal entre el regresor y la respuesta.
-if (t_valor > valor_critico) {
-  # Se agrega el regresor al modelo.
-  dim <- length(regresores_modelo)
-  regresores_modelo[[dim+1]] <- mejor_regresor
-  print(sprintf('Se agrega el Regresor %s al modelo.', mejor_regresor))
+# si satisface el criterio estipulado con la cota de entrada.
+if (abs_t_valor > T_in) {
+  # Se agrega el regresor al modelo en regresores_modelo.
+  regresores_modelo[[length(regresores_modelo)+1]] <- regresor_candidato
+  print(sprintf(
+    'Se ha agregado el regresor %s al modelo pues |to| > T_in',
+    regresor_candidato))
+  print(sprintf('|to| = %f', abs_t_valor))
+  print(sprintf('T_in = %f', T_in))
   # Se elimina el regresor de los regresores disponibles.
-  regresores_disponibles <- regresores_disponibles[regresores_disponibles != mejor_regresor]
+  regresores_disponibles <- regresores_disponibles[
+    regresores_disponibles != regresor_candidato]
+} else {
+  print("Ningún regresor satisface el criterio estipulado.")
 }
 
-
-# 2) Correlaciones Parciales.
-
+# 3] Se hallan la mayores correlaciónes parciales.
 crear_formula <- function(regresores_modelo) {
   # Crea una cadena de texto para una fórmula de modelado lineal
   # Argumentos:
-  #   regresores_modelo: Un vector de caracteres con los nombres de los regresores
+  #   regresores_modelo: Un vector de caracteres con los nombres de los
+  #   regresores
   # Retorna:
   #   Una cadena de texto que representa la fórmula del modelo lineal.
   #
@@ -95,74 +112,86 @@ crear_formula <- function(regresores_modelo) {
 }
 
 while (TRUE){
-  formula_del_modelo <- crear_formula(regresores_modelo)
-  mejor_t_stadistico <- 0
-  mejor_regresor <- NULL
+  mayor_t_stadistico <- 0
+  regresor_candidato <- NULL
   regresores_a_reevaluar <- NULL
+  formula_del_modelo <- crear_formula(regresores_modelo)
   modelo <- NULL
+
   for (regresor in regresores_disponibles) {
-    # Evaluamos "Y ~ Hald$Xi + Hald$Xj" con i fijo y j variable. Caso del primer for dentro del while.
-    formula_string_regresor <- paste(formula_del_modelo, sprintf("+ Hald$%s", regresor))
-    modelin <- lm(as.formula(formula_string_regresor), x = TRUE)
+    # En la primera iteración se evalua "Y ~ Hald$Xi + Hald$Xj" con i fijo y
+    # j variable.
+    formula_string_regresor <- paste(
+      formula_del_modelo, sprintf("+ Hald$%s", regresor))
+    modelo_regresor <- lm(as.formula(formula_string_regresor))
+    # Extraemos el t-estadístico del regresor candidato.
+    t_estadistico <- summary(
+      modelo_regresor)$coefficients[, "t value"][sprintf('Hald$%s', regresor)]
     
-    # Calculamos el t-estadístico.
-    Beta <- modelin$coefficients
-    X <-  modelin$x
-    C <- solve(t(X) %*% X)
-    SSres <- t(Y) %*% Y - t(Beta) %*% t(X) %*% Y
-    num_parametros <- length(regresores_modelo) + 2 # Los regresores en el modelo + B0 y el de la iteración.
-    sigma_estimada_cuadrado <- SSres/(n-num_parametros)
-    t_estadistico <- Beta[num_parametros]/sqrt(sigma_estimada_cuadrado*C[num_parametros, num_parametros])
-    
-    if (abs(t_estadistico) > abs(mejor_t_stadistico)){
-      mejor_t_stadistico <- t_estadistico
-      mejor_regresor <- regresor
-      modelo <- modelin
+    if (abs(t_estadistico) > abs(mayor_t_stadistico)){
+      mayor_t_stadistico <- t_estadistico
+      regresor_candidato <- regresor
+      modelo <- modelo_regresor
     }
 
   }
   
-  df <- df - 1 # Se ha agregado un nuevo parámetro al modelo.
-  valor_critico <- qt(1-alpha/2, df) # Valor Crítico
-  
+  # Se actualizan los grados de libertad pues se va a agregar un nuevo
+  # regresor al modelo.
+  df <- df - 1
+  T_in <- qt(1-alpha_in/2, df) # Cota de entrada.
   regresores_a_reevaluar <- regresores_modelo # Antes de que se agregue el nuevo
-  if (mejor_t_stadistico > valor_critico) {
+
+  if (abs(mayor_t_stadistico) > T_in) {
     # Se agrega el regresor al modelo.
     dim <- length(regresores_modelo)
-    regresores_modelo[[dim+1]] <- mejor_regresor
-    print(sprintf('Se agrega el Regresor %s al modelo.', mejor_regresor))
+    regresores_modelo[[dim+1]] <- regresor_candidato
+    print(sprintf(
+      'Se ha agregado el regresor %s al modelo pues |to| > T_in',
+      regresor_candidato))
+    print(sprintf('|to| = %f', abs(mayor_t_stadistico)))
+    print(sprintf('T_in = %f', T_in))
     # Se elimina el regresor de los regresores disponibles.
-    regresores_disponibles <- regresores_disponibles[regresores_disponibles != mejor_regresor]
+    regresores_disponibles <- regresores_disponibles[
+      regresores_disponibles != regresor_candidato]
   } else {
-    print(sprintf('El t-estadístico del regresor %s, no supera el valor crítico.', mejor_regresor))
+    print('Ninguno de los regresores disponibles supera el valor crítico.')
+    print(regresores_disponibles)
+    print(sprintf('T_in = %f', T_in))
     break
   }
   
+  # 4]
   # Una vez agregado el nuevo regresor, se reevalúan los regresores via el
-  # t-estadístico.
+  # t-estadístico para remover el regresor con el menor t-estadístico siempre y
+  # cuando dicho t-estadístico sea menor que la cota de salida (t_OUT).
+  menor_t_stadistico <- Inf
+  regresor_a_remover <- NULL
   for (regresor in regresores_a_reevaluar){
-    t_estadistico_regresor <- summary(modelo)$coefficients[, "t value"][sprintf('Hald$%s', regresor)]
-    print("t_estadistico_regresor")
-    print(t_estadistico_regresor)
-    print("valor_critico")
-    print(valor_critico)
-    print("df")
-    print(df)
-    if (abs(t_estadistico_regresor) < valor_critico){
-      # Se elimina el regresor de los regresores del modelo
-      regresores_modelo <- regresores_modelo[regresores_modelo != regresor]
-      print(sprintf('Se elimina el Regresor %s del modelo.', regresor))
+    t_estadistico_regresor <- summary(
+      modelo)$coefficients[, "t value"][sprintf('Hald$%s', regresor)]
+
+    if (abs(t_estadistico_regresor) < menor_t_stadistico){
+      menor_t_stadistico <- t_estadistico_regresor
+      regresor_a_remover <- regresor
     }
   }
 
+  T_out <- qt(1-alpha_out/2, df) # Valor crítico, Cota de salida
+  if (abs(menor_t_stadistico) < T_out){
+    # Se elimina el regresor de los regresores del modelo
+    print(sprintf(
+      'Se ha eliminado el regresor %s del modelo pues |to| < T_out',
+      regresor_a_remover))
+    print(sprintf('|to| = %f', abs(menor_t_stadistico)))
+    print(sprintf('T_out = %f', T_out))
+    regresores_modelo <- regresores_modelo[
+      regresores_modelo != regresor_a_remover]
+    # Se agrega a los regresores disponibles.
+    dim <- length(regresores_disponibles)
+    regresores_disponibles[[dim+1]] <- regresor_a_remover
+    df <- df + 1 # Se obtiene un grado de libertad al prescindir de un regresor.
+  }
 }
 
-
-print(paste("El modelo final es:", crear_formula((regresores_modelo))))
-
-
-
-
-
-
-
+print(paste("La ecuación final es:", crear_formula((regresores_modelo))))
